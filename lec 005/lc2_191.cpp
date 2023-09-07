e0b1c2bf49f7297fa15e6e9b436d53db79021fb3
Kabindra Borah <kabindraborah68@gmail.com>
2023-09-08 00:41:27 +0530

    Update lc2_191.cpp

--- lec 005/lc2_191.cpp
+++ lec 005/lc2_191.cpp
@@ -5,30 +5,23 @@ using namespace std;
 class Solution {
 public:
     int hammingWeight(uint32_t n) {
+
         int count = 0;
-        
-        while (n != 0) {
+        while (n > 0) {
             if (n & 1) {
                 count++;
             }
-            n = n >> 1;
+            n >>= 1;
         }
-        
         return count;
     }
 };
 
 int main() {
-    Solution solution;
-    uint32_t n;
-
-    // Example usage
-    cout << "Enter a 32-bit unsigned integer: ";
-    cin >> n;
-    
-    int result = solution.hammingWeight(n);
-    
-    cout << "Number of 1 bits (Hamming weight): " << result << endl;
-    
-    return 0;
+    Solution sol;
+    int n;
+   cout<<"Enter a base 10 number";
+   cin>>n;
+    int count = sol.hammingWeight(n);
+    cout << count << endl; 
 }
