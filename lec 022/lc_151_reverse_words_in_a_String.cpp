27d17a7030453c371bfa25d183af5cd796734f53
Kabindra Borah <kabindraborah68@gmail.com>
2023-09-28 21:42:07 +0530

    Add files via upload

---
+++ lec 022/lc_151_reverse_words_in_a_String.cpp
@@ -0,0 +1,30 @@
+#include<iostream>
+#include<string.h>
+using namespace std;
+
+class Solution {
+public:
+    string reverseWords(string s) {
+         
+        int n=s.size(),i=0;
+        string ans="";
+        while(i<n)
+        {
+            string tm="" ; // empty string
+           while( s[i]==' '&& i<n)
+              i++;
+           while( s[i]!=' '&& i<n)
+           {
+          tm+=s[i];
+              i++;
+           }
+           if(tm.size()>0){
+               if(ans.size()==0)
+               ans=tm;
+               else
+               ans= tm+" "+ans;
+           }
+        }
+        return ans;
+    }
+};
